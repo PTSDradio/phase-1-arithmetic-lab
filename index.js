654f6d2a3f729b523dcfa363dfa6f998bcef1006
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-06-12 15:21:13 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2
+const num2 =31
+const num3 = 29
+const num4 =5
+const multiply= num1*num2
+let random = Math.floor(Math.random())+1
+const mod = num3%num4
+const max = Math.max(20)
